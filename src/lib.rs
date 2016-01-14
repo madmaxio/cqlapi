0742665963425abfad6f758a2886afff5b3ea748
Maxim <sprayrules@gmail.com>
2016-01-14 18:36:12 +0300

    API

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,9 @@ extern crate rustcql;
 pub mod proto;
 
 pub use rustcql::Connection;
+pub use rustcql::connect;
 pub use rustcql::shared::Consistency;
+pub use rustcql::shared::Consistency::Quorum;
 pub use rustcql::shared::Response;
 pub use rustcql::shared::ResultBody;
 pub use rustcql::shared::Row;
