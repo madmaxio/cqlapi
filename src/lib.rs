994f3e9f50d34c2f781dc85976b4b7116f327537
Maxim <sprayrules@gmail.com>
2016-01-14 17:35:10 +0300

    cqlapi

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,13 @@ extern crate rustcql;
 
 #[warn(missing_docs)]
 pub mod proto;
+
+pub use rustcql::Connection;
+pub use rustcql::shared::Consistency;
+pub use rustcql::shared::Response;
+pub use rustcql::shared::ResultBody;
+pub use rustcql::shared::Row;
+pub use rustcql::shared::BatchQuery;
+pub use rustcql::shared::Column;
+
+pub use proto::*;
